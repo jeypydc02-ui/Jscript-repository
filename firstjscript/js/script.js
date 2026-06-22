8808851f1c96a3e5c6ae4bee9d1b1075890e8baa
jeypydc02-ui <jeypydc02@gmail.com>
2026-06-22 21:32:16 +0800

    Arrays

--- firstjscript/js/script.js
+++ firstjscript/js/script.js
@@ -1,91 +1,86 @@
-// WORD LENGTH
-let word = "Hello Philippines";
-let len = word.length;
-alert(len);
-console.log(len);
+//ARRAYSS
+
+//Arrays of Strings
+let F1Teams = ["Mclaren ", "Ferarri ", "Redbull Racing ", "Aston Martin "];
+alert(`The lenght Arrays of F1Teams's variable is ${F1Teams.length}.`);
+
+console.log(F1Teams);
+console.log(F1Teams.length);
+
+alert(`The Arrays of index 1 is ${F1Teams[1]}.`);
+console.log(`The Arrays of index 1 is ${F1Teams[1]}.`);
+
+F1Teams[F1Teams.length-2] = "Alpine Motosport ";
+console.log(F1Teams[F1Teams.length-2]);
+
+console.log(F1Teams);
+
+F1Teams[0] = "Audi Motosport ";
+console.log(F1Teams);
+
+//Add using push 
+F1Teams.push('Kick Sauber ');
+F1Teams.push("and Cadillac");
+
+//Add using unshift
+F1Teams.unshift('Racing Bulls ');
+F1Teams.unshift('HAAS ');
+
+console.log(`Here are the teams that qualified as teams in Formula 1 2026 "${F1Teams}."`);
+
+console.log(F1Teams.length);
+
+F1Teams.length = 5;
+console.log(F1Teams);
 
 
-let word1 = "Jeypy";
-alert(word1.length);
-console.log(word1.length);
+let named = ["Thomas", "John PaulC", "Kyle Cedric", "John PaulDC", "Jesus", "Richmond"];
 
-//String Indices
-let name = "Joshua";
-console.log(name[0]);
-console.log(name[1]);
-console.log(name[2]);
-console.log(name[3]);
-console.log(name[4]);
-console.log(name[5]);
+console.log(named.length);
+console.log(named);
 
-name[name.length-6];
-name[name.length-5];
-name[name.length-4];
-name[name.length-3];
-name[name.length-2];
-name[name.length-1];
+named.length = 5;
+console.log(named);
+named.pop();
+console.log(named);
+named.shift();
+console.log(named);
 
-console.log(
- `"${name[name.length - 6]}${name[name.length - 5]}${name[name.length - 4]}${name[name.length - 3]}${name[name.length - 2]}${name[name.length - 1]}"`
-);
+let namess = ["Ian", "Ceejhay", "Eunich", "Joshua", "Mika", "Jeyp"];
+let sliced = namess.slice(0, 3);
+console.log(sliced);
+let sliced2 = namess.slice(0, 1);
+console.log(sliced2);
 
 
-//Different Methods
-
-let names = " John Paul S. Dela Cruz ";
-console.log(names.length);
-console.log(names.toUpperCase());
-console.log(names.toLowerCase());
-console.log(names.trim());
-console.log(names.trimEnd());
-console.log(names.trimStart());
-
-console.log(names.trim().length);
-console.log(names.trimEnd().length);
-console.log(names.trimStart().length);
-
-console.log(names.replace("Dela Cruz", "Salonga"));
-console.log(names.replaceAll(names, "Lewis Hamilton"));
-console.log(names);
-
-let newName = "Ian Kenneth Siangio";
-
-// slice from start
-console.log(newName.slice(0, 3));         // "Ian"
-console.log(newName.slice(0, 3).length);  // 3
-
-// slice full name using length
-console.log(newName.slice(0, newName.length));         // "Ian Kenneth Siangio"
-console.log(newName.slice(0, newName.length).length);  // 19
-
-console.log(newName.slice(newName.length - 19, newName.length));      
-console.log(newName.slice(newName.length - 19, newName.length).length);                          
-
-//String Templace Literals
-let words = `String "Literals"`;
-console.log(words);
-
-let kangaranan = "Jampol";
-console.log(`Hi, my name is ${kangaranan}. I want to become a Software Engineer someday. I'm gonna be a Lebron James in my Profession`);
-
-let numss1 =5, numss2 = 10
-let sum = numss1 + numss2;
-console.log(`${numss1} + ${numss2} = ${sum}`);
+//Arrays of Numbers
+let ages = [90, 32, 23, 33, 52];
+console.log(ages);
+ages[1] = 44;
+console.log(ages);
 
 
 //Tutorial Challenge
-let who = "Jeyp";
-let who2 = "Zere May";
-let pronoun= "She";
-let drinks = "coffee";
 
+let firstName = ["John Paul", "Thomas Guiller", "Richmond", "John Paul", "Jesus", "Kyle Cedric"];
+let lastName = ["Dela Cruz", "Cruz", "Garaza", "Cruz", "Gamboa", "Espin"];
+let age = [21, 23, 20, 23, 22, 22];
 
-console.log(`${who2}, the frienly neighbor, waved at ${who} as ${pronoun} walked by ${who}'s house. ${who} smiled back and invited ${who2} in for a cup of ${drinks}. `)
+let index = 1;
+console.log(`Index       :${index}`);
+console.log(`First Name  :${firstName[index]}`);
+console.log(`Last Name   :${lastName[index]}`);
+console.log(`Age         :${age[index]}`);
+console.log("");
 
-
-
-
-
-
-
-
+ index = 0;
+console.log(`Index       :${index}`);
+console.log(`First Name  :${firstName[index]}`);
+console.log(`Last Name   :${lastName[index]}`);
+console.log(`Age         :${age[index]}`);
+console.log("");
+ index = 3;
+console.log(`Index       :${index}`);
+console.log(`First Name  :${firstName[index]}`);
+console.log(`Last Name   :${lastName[index]}`);
+console.log(`Age         :${age[index]}`);

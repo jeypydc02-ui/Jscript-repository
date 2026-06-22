5f96df25ad99b7553d09541a35e9d950bbed285d
jeypydc02-ui <jeypydc02@gmail.com>
2026-06-22 13:06:55 +0800

    Strings

--- firstjscript/js/script.js
+++ firstjscript/js/script.js
@@ -1,95 +1,86 @@
+// WORD LENGTH
+let word = "Hello Philippines";
+let len = word.length;
+alert(len);
+console.log(len);
 
 
+let word1 = "Jeypy";
+alert(word1.length);
+console.log(word1.length);
 
-//Convert String to Numbers
-let num1 = "10";
-let num2 = 20;
-console.log(num1 + num2); //Output: 1020
-alert(num1 + num2); //Output: 1020
-document.getElementById("results").innerHTML = num1 + num2; //Output: 1020
+//String Indices
+let name = "Joshua";
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name[5]);
 
-let num3 = 44;
-let num4 = 1;
-console.log(num3 + num4); //Output: 45
-alert(num3 + num4);
-document.getElementById("results2").innerHTML = num3 + num4; //Output: 45
+name[name.length-6];
+name[name.length-5];
+name[name.length-4];
+name[name.length-3];
+name[name.length-2];
+name[name.length-1];
 
-let num5 = "44";
-let num6 =23;
-console.log(parseInt(num5) + num6);
-
-
-let num = parseInt("123");
-let num7 = 12;
-console.log(num + num7); //Output: 135
-
-
-let num8 = "889sdfsefse";
-let num9 = 11;
-num8 = parseInt(num8);
-console.log(num8 + num9); //Output: 900
-
-let num10 = "15.98";
-num10 = parseInt(num10);
-console.log(num10); //Output: 15
-num10 = parseFloat("15.98");
-console.log(num10); //Output: 15.98
-
-//Convert String to Numbers using Math.round()
-let num11 = "15.98";
-num11 = Math.round(num11);
-console.log(num11); //Output: 16
+console.log(
+ `"${name[name.length - 6]}${name[name.length - 5]}${name[name.length - 4]}${name[name.length - 3]}${name[name.length - 2]}${name[name.length - 1]}"`
+);
 
 
-//PEMDAS
-let xx = 5;
-let yy = 10;
-console.log((xx+yy)/xx); //Output: 3
+//Different Methods
 
+let names = " John Paul S. Dela Cruz ";
+console.log(names.length);
+console.log(names.toUpperCase());
+console.log(names.toLowerCase());
+console.log(names.trim());
+console.log(names.trimEnd());
+console.log(names.trimStart());
 
-//Shorthand Operators
-let num12 = 5
-num12 += 10; //num12 = num12 + 10
-console.log(num12); //Output: 15
+console.log(names.trim().length);
+console.log(names.trimEnd().length);
+console.log(names.trimStart().length);
 
-let num13 = 5
-num13 -= 10;
-console.log(num13); //Output: -5
+console.log(names.replace("Dela Cruz", "Salonga"));
+console.log(names.replaceAll(names, "Lewis Hamilton"));
+console.log(names);
 
-let num14 = 5
-num14 *= 10;
-console.log(num14); //Output: 50
+let newName = "Ian Kenneth Siangio";
 
-//Increment and Decrement Operators
+// slice from start
+console.log(newName.slice(0, 3));         // "Ian"
+console.log(newName.slice(0, 3).length);  // 3
 
-//Prefix
-let x = 10;
-console.log(++x); //Output: 11
-console.log(x); //Output: 11
+// slice full name using length
+console.log(newName.slice(0, newName.length));         // "Ian Kenneth Siangio"
+console.log(newName.slice(0, newName.length).length);  // 19
 
-//PostFix
-let y = 10;
-console.log(y++); //Output: 10
-console.log(y); //Output: 11
+console.log(newName.slice(newName.length - 19, newName.length));      
+console.log(newName.slice(newName.length - 19, newName.length).length);                          
+
+//String Templace Literals
+let words = `String "Literals"`;
+console.log(words);
+
+let kangaranan = "Jampol";
+console.log(`Hi, my name is ${kangaranan}. I want to become a Software Engineer someday. I'm gonna be a Lebron James in my Profession`);
+
+let numss1 =5, numss2 = 10
+let sum = numss1 + numss2;
+console.log(`${numss1} + ${numss2} = ${sum}`);
 
 
 //Tutorial Challenge
-// GWA
-let Websys = parseFloat("4.00")
-let Database = parseFloat("3.50")
-let WebProgramming = parseFloat("3.75")
-let MobileProgramming = parseFloat("3.25")
-let Adinfosec = parseFloat("3.75")
-let GWA = (Websys + Database + WebProgramming + MobileProgramming + Adinfosec);
-GWA /=5
+let who = "Jeyp";
+let who2 = "Zere May";
+let pronoun= "She";
+let drinks = "coffee";
 
-console.log("Web Systems and Technologies   : " + Websys);
-console.log("Database Management Systems    : " + Database);
-console.log("Web Programming                : " + WebProgramming);
-console.log("Mobile Programming             : " + MobileProgramming);
-console.log("Advanced Information Security  : " + Adinfosec);
-console.log();
-console.log("General Weight Average         : " + GWA)
+
+console.log(`${who2}, the frienly neighbor, waved at ${who} as ${pronoun} walked by ${who}'s house. ${who} smiled back and invited ${who2} in for a cup of ${drinks}. `)
 
 
 

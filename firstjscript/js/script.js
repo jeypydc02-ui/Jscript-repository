2819cf8b56d3dbb02fde28b338bd22b2eadedcb7
jeypydc02-ui <jeypydc02@gmail.com>
2026-06-23 16:53:21 +0800

    Conditional-Statement

--- firstjscript/js/script.js
+++ firstjscript/js/script.js
@@ -1,86 +1,102 @@
-//ARRAYSS
+//Conditional Statement
+let x =10;
 
-//Arrays of Strings
-let F1Teams = ["Mclaren ", "Ferarri ", "Redbull Racing ", "Aston Martin "];
-alert(`The lenght Arrays of F1Teams's variable is ${F1Teams.length}.`);
-
-console.log(F1Teams);
-console.log(F1Teams.length);
-
-alert(`The Arrays of index 1 is ${F1Teams[1]}.`);
-console.log(`The Arrays of index 1 is ${F1Teams[1]}.`);
-
-F1Teams[F1Teams.length-2] = "Alpine Motosport ";
-console.log(F1Teams[F1Teams.length-2]);
-
-console.log(F1Teams);
-
-F1Teams[0] = "Audi Motosport ";
-console.log(F1Teams);
-
-//Add using push 
-F1Teams.push('Kick Sauber ');
-F1Teams.push("and Cadillac");
-
-//Add using unshift
-F1Teams.unshift('Racing Bulls ');
-F1Teams.unshift('HAAS ');
-
-console.log(`Here are the teams that qualified as teams in Formula 1 2026 "${F1Teams}."`);
-
-console.log(F1Teams.length);
-
-F1Teams.length = 5;
-console.log(F1Teams);
-
-
-let named = ["Thomas", "John PaulC", "Kyle Cedric", "John PaulDC", "Jesus", "Richmond"];
-
-console.log(named.length);
-console.log(named);
-
-named.length = 5;
-console.log(named);
-named.pop();
-console.log(named);
-named.shift();
-console.log(named);
-
-let namess = ["Ian", "Ceejhay", "Eunich", "Joshua", "Mika", "Jeyp"];
-let sliced = namess.slice(0, 3);
-console.log(sliced);
-let sliced2 = namess.slice(0, 1);
-console.log(sliced2);
-
-
-//Arrays of Numbers
-let ages = [90, 32, 23, 33, 52];
-console.log(ages);
-ages[1] = 44;
-console.log(ages);
-
-
-//Tutorial Challenge
-
-let firstName = ["John Paul", "Thomas Guiller", "Richmond", "John Paul", "Jesus", "Kyle Cedric"];
-let lastName = ["Dela Cruz", "Cruz", "Garaza", "Cruz", "Gamboa", "Espin"];
-let age = [21, 23, 20, 23, 22, 22];
-
-let index = 1;
-console.log(`Index       :${index}`);
-console.log(`First Name  :${firstName[index]}`);
-console.log(`Last Name   :${lastName[index]}`);
-console.log(`Age         :${age[index]}`);
+console.log(x =="10");
+console.log(x==="10");
+console.log(x===parseInt("10"));
 console.log("");
+console.log(x !="10");
+console.log(x !=="10");
+console.log(x !== parseInt("10"));
 
- index = 0;
-console.log(`Index       :${index}`);
-console.log(`First Name  :${firstName[index]}`);
-console.log(`Last Name   :${lastName[index]}`);
-console.log(`Age         :${age[index]}`);
-console.log("");
- index = 3;
-console.log(`Index       :${index}`);
-console.log(`First Name  :${firstName[index]}`);
-console.log(`Last Name   :${lastName[index]}`);
-console.log(`Age         :${age[index]}`);
+let a = "Hello World";
+let b = "hello world";
+console.log(a == b);
+
+
+ //age conditional statement
+
+let age = -19;           
+let ageCategory = [
+ "Senior Citizen", //0, 60+
+ "Adult",          //1, 40-59
+ "Young Adult",    //2, 20-39
+ "Teenager",       //3, 13-19
+ "Kid",            //4, 4-12
+ "Toddler",        //5, 2-3
+ "Baby"];          //6, 0-1
+
+
+if(age >=60) {
+    console.log(`"You are ${ageCategory[0]}."`);
+
+}else if(age >=40) {
+    console.log(`You are ${ageCategory[1]}.`);
+
+}else if(age >=20) {
+    console.log(`You are ${ageCategory[2]}.`);
+
+}else if(age >=13) {
+    console.log(`You are ${ageCategory[3]}.`);
+
+}else if (age >= 4) {
+    console.log(`You are ${ageCategory[4]}.`);
+
+}else if (age >= 2) {
+    console.log(`You are ${ageCategory[5]}.`);
+
+}else if (age>=0) {
+    console.log(`You are ${ageCategory[6]}.`);
+
+}else if (age < 0) {
+    console.log(`Age doesn't exist`);
+}else {
+     console.log(`Age doesn't exist`);
+}
+
+
+
+let mathGrade = 96;
+let scienceGrade = 93;
+let englishGrade = 90;
+let filipinoGrade =87;
+let computerGrade = 99;
+
+let gradeCategory = ["With Highest Honor", "With High Honor", "With Honor", "Passed", "Failed"];
+
+let gwa = mathGrade + scienceGrade + englishGrade + filipinoGrade + computerGrade;
+gwa /= 5;
+
+let fgwa = parseFloat(gwa.toFixed(2));
+
+console.log("GRADES:")
+console.log(`Math                         : ${mathGrade}`);
+console.log(`Science                      : ${scienceGrade}`);
+console.log(`English                      : ${englishGrade}`);
+console.log(`Filipino                     : ${filipinoGrade}`);
+console.log(`Computer                     : ${computerGrade}`);
+console.log(`General Weight Average       : ${fgwa.toFixed(2)}`);
+
+if (fgwa > 100) {
+    console.log(`Invalid Grade`);
+
+}else if (fgwa >=98) {
+    console.log(`You are ${gradeCategory[0]}.`);
+
+}else if (fgwa >= 95) {
+    console.log(`You are ${gradeCategory[1]}.`);
+
+}else if (fgwa >= 90) {
+    console.log(`You are ${gradeCategory[2]}.`);
+
+}else if (fgwa >= 75) {
+    console.log(`You are ${gradeCategory[3]}.`);
+
+}else if (fgwa < 75) {
+    console.log(`You are ${gradeCategory[4]}.`);
+
+}else if (fgwa < 0) {
+    console.log(`Invalid Grade`);
+} else {
+     console.log(`Invalid Grade`);
+}

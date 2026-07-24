1707f0bff7f3deb1764658cf2f674a948f4a773c
jeypydc02-ui <jeypydc02@gmail.com>
2026-07-24 20:20:00 +0800

    DOMTutorialChallenge

--- firstjscript/js/script.js
+++ firstjscript/js/script.js
@@ -1,87 +1,28 @@
-//Document Object Model
-
-//Using GetElementById
-let result = document.getElementById("results");
-console.log(result);
 
 
-//Using QuerySelector
-let result2 = document.querySelector(".container2 .results");
-console.log(result2);
-
-//Using GetElementByClassName
-let result3 = document.getElementsByClassName("results");
-console.log(result3);
-
-//Using QuerySelctorAll
-let allClass = document.querySelectorAll(".container2 .results");
-console.log(allClass);
-
-let innerHTML = document.querySelector(".container #results4").innerHTML = `This is the new innerHTML`;
-console.log(innerHTML);
-
-let innerText = document.querySelector(".container #results4");
-console.log(innerText);
-
-console.log("");
-//Stored in an Array
-
-let allResults = document.querySelectorAll(".container2 .results");
+let body = document.querySelector("body");
+body.style.backgroundColor = "black";
+body.style.margin = "0px";
+body.style.textAlign = "center";
 
 
+let img = document.querySelector("img");
+    img.src="https://scontent.fmnl9-4.fna.fbcdn.net/v/t39.30808-6/481011215_1021316016476607_3189068972814539585_n.jpg?stp=dst-jpg_tt6&cstp=mx947x960&ctp=s947x960&_nc_cat=107&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeF6ZXnm-4wfhC1QuzMM-aoq0BIiaqVtI63QEiJqpW0jrZrS8U84S5XZ-osEabnKYN7KBHuFlsYj-pQFc6wWIO89&_nc_ohc=_OBe85qD9eAQ7kNvwFK9PPy&_nc_oc=Adqi6Rhtx8B5UDySO1rWXyt5Y069Nr4dGalmOR9y4zMsAhJtxBnbpFuIq7J3cojR0NE&_nc_zt=23&_nc_ht=scontent.fmnl9-4.fna&_nc_gid=_qtVsBPFUih3x3BAPrZz9w&_nc_ss=7b2a8&oh=00_AQCzX4WTN9oWDvkR0NWyeV0cJ3x48mPvA062rX7z1bLZ_A&oe=6A691147";
+    img.style.height= "100px";
+    img.style.marginTop = "50px";
 
-console.log(allResults[0]);
-console.log(allResults[1]);
-console.log(allResults[2]);
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = "John Paul S. Dela Cruz";
+    h1.style.color = "white";
+    h1.style.fontFamily= "montseratt";
 
-console.log("");
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "gray";
+    div.style.height = "200px";
+    div.style.marginBottom = "0px";
+    div.style.padding = "20px";
 
-//Accessing Array for Specific Element
-let firstResults = allResults[0].innerHTML = `This is the new innerHTML for first element`;
-let secondResults = allResults[1].innerHTML = `This is the new innerHTML for second element`;
-let thirdResults = allResults[2].innerHTML = `This is the new innerHTML for third element`;
-
-console.log(allResults[0]);
-console.log(firstResults);
-console.log(secondResults);
-console.log(thirdResults);
-
-
-
-
-
-
-
-
-
-//MANIPULATE ATTRIBUTES
-
-let textInput = document.querySelector("#input");
-textInput.placeholder = `Enter your first name`;
-
-let textButton = document.querySelector("#btn");
-textButton.textContent = `Pindotin ako`;
-
-
-
-
-//Image
-
-let imagee = document.querySelector("#image");
-imagee.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS28LgmC2T4_ftwGSxmAZAshdkcJ41C8NMqqBgjkO7JU-eJnRMfbPvKx5k&s=10"
-
-let changeLink = document.querySelector("#link");
-
-changeLink.href = "https://www.instagram.com/jppdlcrz/"
-changeLink.target = "_blank";
-
-let changeBody = document.querySelector("body");
-changeBody.style.backgroundColor = "lightgray";
-
-let changeH1 = document.querySelector("#results");
-changeH1.style.textAlign = "center";
-changeH1.style.fontFamily = "Arial, Helvetica, sans-serif";
-changeH1.style.color = "maroon";
-changeH1.style.borderBottom = "2px solid maroon";
-
-
+    let p = document.querySelector("p");
+    p.textContent = `I'm John Paul S. Dela Cruz, a BSIT Student from National University of Philippines - Manila. I lived in Sitio Old Boso-Boso Brgy. San Jose, Antipolo City, Rizal, Philippines and I want to become a Full Stack Software Developer in the future.`;
+    p.style.color = "white";
+    p.style.margin = "20px";
